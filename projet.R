6bf9c1d6721a05761bb85498f33137ef1ebb357e
Laurie232 <lasqu@ulaval.ca>
2025-11-25 12:04:04 -0500

    graphique modifié

--- projet.R
+++ projet.R
@@ -40,28 +40,29 @@ library(ggplot2)
 
 # Méthode plot.context_table
 
-plot.context_table <- function(x, ...) {
-  # x est l'objet de classe "context_table"
+plot.context_table <- function(obj, ...) {
+  # Vérifier la classe
+  stopifnot(inherits(obj, "context_table"))
   
-  df <- data.frame(
-    label   = "proportion",
-    estimate = x$prop_estimee,
-    ic_inf   = x$ic_inf,
-    ic_sup   = x$ic_sup
+  # Construire un tableau 2x2 : Observé vs sous H0
+  tab <- rbind(
+    Observé = c(Succès = obj$x,
+                Échec  = obj$n - obj$x),
+    "Sous H0" = c(Succès = obj$n * obj$p0,
+                  Échec  = obj$n * (1 - obj$p0))
   )
   
-  ggplot2::ggplot(df, ggplot2::aes(x = label, y = estimate)) +
-    ggplot2::geom_point() +
-    ggplot2::geom_errorbar(
-      ggplot2::aes(ymin = ic_inf, ymax = ic_sup),
-      width = 0.1
-    ) +
-    ggplot2::coord_cartesian(ylim = c(0, 1)) +
-    ggplot2::labs(
-      x = NULL,
-      y = "Proportion",
-      title = "Résultat du test de proportion"
-    )
+  # Tracer la mosaïque
+  graphics::mosaicplot(
+    tab,
+    main = "Test de proportion",
+    xlab = "",
+    ylab = "Fréquence relative",
+    color = TRUE,
+    ...
+  )
+  
+  invisible(obj)
 }
 
 obj <- prop_test_context(12, 20, p0 = 0.5)
